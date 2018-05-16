1e9e0bc38944815fcb5f90c21957b41f8de6c111
ivranjes <ivan.vranjes@t.ht.hr>
2018-05-16 21:52:17 +0200

    composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        schema::defaultStringLenght(191);
+        schema::defaultStringLength(191);
     }
 
     /**
